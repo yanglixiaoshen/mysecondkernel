cdee0abb0cf67c2766531c94ac8aaab73410d71d
yangli_ mac <yangliTXWD@163.com>
2017-04-11 20:35:12 +0800

    Changes to be committed: 	new file:   mypcb.h

---
+++ mypcb.h
@@ -0,0 +1,29 @@
+/*
+ *  linux/mykernel/mypcb.h
+ *
+ *  Kernel internal PCB types
+ *
+ *  Copyright (C) 2013  Mengning
+ *
+ */
+
+#define MAX_TASK_NUM        4
+#define KERNEL_STACK_SIZE   1024*2 # unsigned long
+/* CPU-specific state of this task */
+struct Thread {
+    unsigned long		ip;
+    unsigned long		sp;
+};
+
+typedef struct PCB{
+    int pid;
+    volatile long state;	/* -1 unrunnable, 0 runnable, >0 stopped */
+    unsigned long stack[KERNEL_STACK_SIZE];
+    /* CPU-specific state of this task */
+    struct Thread thread;
+    unsigned long	task_entry;
+    struct PCB *next;
+}tPCB;
+
+void my_schedule(void);
+
